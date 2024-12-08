38870119a31dac51fa480fc7ec468668e7bb9426
Raresreras <89092397+Raresreras@users.noreply.github.com>
2024-12-08 15:26:26 +0200

    Rewritten the project in express js and added websocket

--- server.js
+++ server.js
@@ -1,55 +1,41 @@
-import http from 'http';
-import fs from 'fs/promises'
-import url from 'url'
-import path from 'path'
-import SSE from 'sse'
+import express from 'express';
 const port = 8000;
 
-let bodyText
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended : false}));
+app.use(express.json())
 
-//might rewrite API from vanilla nodejs to Express.js
+//WEBSOCKET
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import http from 'http';
+import { WebSocketServer } from 'ws';
 
-const server = http.createServer(async (req, res) =>{
-    let filepath;
-    if (req.method === 'GET'){
-        if (req.url === '/question') {
-            filepath = path.join(__dirname, '/public', '/question.html');
-        }
-        else {
-            filepath = path.join(__dirname, '/public', '/index.html');
-        }
-    }
-    if (req.method === "POST"){
-        //POST black magic
-        let body = '';
-        req.on('data', chunk => {
-            body += chunk.toString(); // Convert Buffer to string
-        });
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
 
-        // When all data has been received
-        req.on('end', () => {
-            //body formatting
-            //TODO! split intro multiple functions + change %3F into ?
-            bodyText = String(body).replaceAll('+', ' ').replace('question=', '');
-            console.log(bodyText);
-
-            //TODO! implement SSE
-        })
-
-        //defaulting to the question path
-        filepath = path.join(__dirname, '/public', '/question.html');
-    }
-    
-    const data = await fs.readFile(filepath);
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
-    res.write(data);
-    res.end();
+wss.on('connection', (ws) =>{
+    console.log("NEW CLIENT CONNECTED");
 });
 
-server.listen(port, () => {
-    console.log('Server running on port ' + port);
+//IMPLEMENTATION
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.render('index', {questionText : 'Awaiting question'}); //questionText will be used to display the question
 });
+
+app.get('/question', (req, res) =>{
+    res.status(200);
+    res.render('question');
+});
+
+app.post('/question', async (req, res) =>{
+    console.log(req.body);
+    res.redirect('question');
+});
+
+
+server.listen(port);
+console.log('Server running on port ' + port);
+
