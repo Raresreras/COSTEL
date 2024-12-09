7777bbfedba40e4904d36d7cea5663b23c4163b3
Raresreras <89092397+Raresreras@users.noreply.github.com>
2024-12-09 09:33:36 +0200

    Added proper websocket communication

--- server.js
+++ server.js
@@ -17,10 +17,11 @@ wss.on('connection', (ws) =>{
 
     ws.on('message', (message) =>{
         console.log('received: ' + message);
-
+        
+        //broadcast message to all clients
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                client.send(JSON.parse(message)); 
             }
         });
     });
